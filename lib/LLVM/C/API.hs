d576a2a7deb50f9af96403f7ce9f0158aafb1091
Luc Tielen <luc.tielen@gmail.com>
2022-05-23 20:48:54 +0200

    Split LLVM.C modules into API + Bindings

---
+++ lib/LLVM/C/API.hs
@@ -0,0 +1,85 @@
+module LLVM.C.API
+  ( Context
+  , Module
+  , TargetData
+  , Type
+  , mkContext
+  , mkModule
+  , getTargetData
+  , sizeOfType
+
+  , mkVoidType
+  , mkIntType
+  , mkPointerType
+  , mkStructType
+  , mkArrayType
+  , mkFunctionType
+  ) where
+
+import Data.Word
+import Foreign.C
+import Foreign.Ptr
+import Foreign.ForeignPtr
+import Foreign.Marshal.Array
+import Control.Exception
+import Data.Text (Text)
+import qualified Data.Text as T
+import LLVM.C.Bindings
+
+
+mkVoidType :: ForeignPtr Context -> IO (Ptr Type)
+mkVoidType ctx = withForeignPtr ctx llvmVoidTypeInContext
+
+mkIntType :: ForeignPtr Context -> Word32 -> IO (Ptr Type)
+mkIntType ctx bits = withForeignPtr ctx $ \c ->
+  case bits of
+    1 -> llvmI1TypeInContext c
+    8 -> llvmI8TypeInContext c
+    16 -> llvmI16TypeInContext c
+    32 -> llvmI32TypeInContext c
+    64 -> llvmI64TypeInContext c
+    _ -> llvmIntTypeInContext c (CUInt bits)
+
+mkPointerType :: Ptr Type -> IO (Ptr Type)
+mkPointerType pointeeTy =
+  llvmPointerTypeInContext pointeeTy 0
+
+mkStructType :: ForeignPtr Context -> [Ptr Type] -> Bool -> IO (Ptr Type)
+mkStructType ctx tys packed =
+  withForeignPtr ctx $ \c ->
+    withArray tys $ \tyArray -> do
+      let count = CUInt $ fromIntegral $ length tys
+          packed' = CBool (if packed then 1 else 0)
+      llvmStructTypeInContext c tyArray count packed'
+
+mkFunctionType :: Ptr Type -> [Ptr Type] -> IO (Ptr Type)
+mkFunctionType retTy argTys =
+  withArray argTys $ \argTyArray -> do
+    let argCount = CUInt $ fromIntegral $ length argTys
+        isVarArg = CBool 0
+    llvmFunctionTypeInContext retTy argTyArray argCount isVarArg
+
+mkArrayType :: Ptr Type -> Word32 -> IO (Ptr Type)
+mkArrayType elemTy count =
+  llvmArrayTypeInContext elemTy (CUInt count)
+
+mkContext :: IO (ForeignPtr Context)
+mkContext = mask_ $ do
+  ctx <- llvmContextCreate
+  newForeignPtr llvmContextDispose ctx
+
+mkModule :: ForeignPtr Context -> Text -> IO (ForeignPtr Module)
+mkModule ctx name =
+  withCString (T.unpack name) $ \name' -> do
+    withForeignPtr ctx $ \c -> mask_ $ do
+      llvmModule <- llvmCreateModuleWithName name' c
+      newForeignPtr llvmDisposeModule llvmModule
+
+getTargetData :: ForeignPtr Module -> IO (Ptr TargetData)
+getTargetData llvmModule =
+  withForeignPtr llvmModule llvmGetTargetData
+
+sizeOfType :: Ptr TargetData -> Ptr Type -> IO Word64
+sizeOfType td ty = do
+  CSize byteSize <- llvmSizeOfType td ty
+  pure byteSize
--- lib/LLVM/C/Bindings.hs
+++ lib/LLVM/C/Bindings.hs
@@ -3,30 +3,29 @@ module LLVM.C.Bindings
   , Module
   , TargetData
   , Type
-  , mkContext
-  , mkModule
-  , getTargetData
-  , sizeOfType
-
-  , mkVoidType
-  , mkIntType
-  , mkPointerType
-  , mkStructType
-  , mkArrayType
-  , mkFunctionType
+  , llvmContextCreate
+  , llvmContextDispose
+  , llvmCreateModuleWithName
+  , llvmDisposeModule
+  , llvmGetTargetData
+  , llvmSizeOfType
+  , llvmVoidTypeInContext
+  , llvmI1TypeInContext
+  , llvmI8TypeInContext
+  , llvmI16TypeInContext
+  , llvmI32TypeInContext
+  , llvmI64TypeInContext
+  , llvmIntTypeInContext
+  , llvmPointerTypeInContext
+  , llvmStructTypeInContext
+  , llvmArrayTypeInContext
+  , llvmFunctionTypeInContext
   ) where
 
-import Data.Word
 import Foreign.C
 import Foreign.Ptr
-import Foreign.ForeignPtr
-import Foreign.Marshal.Array
-import Control.Exception
-import Data.Text (Text)
-import qualified Data.Text as T
 
 
--- TODO: split into multiple modules: bindings.hs, api.hs, ...
 -- TODO: use ReaderT (ForeignPtr Context) IO a
 
 data Context
@@ -85,60 +84,3 @@ foreign import ccall unsafe "LLVMArrayType" llvmArrayTypeInContext
 foreign import ccall unsafe "LLVMFunctionType" llvmFunctionTypeInContext
   :: Ptr Type -> Ptr (Ptr Type) -> CUInt -> CBool -> IO (Ptr Type)
 
-
-mkVoidType :: ForeignPtr Context -> IO (Ptr Type)
-mkVoidType ctx = withForeignPtr ctx llvmVoidTypeInContext
-
-mkIntType :: ForeignPtr Context -> Word32 -> IO (Ptr Type)
-mkIntType ctx bits = withForeignPtr ctx $ \c ->
-  case bits of
-    1 -> llvmI1TypeInContext c
-    8 -> llvmI8TypeInContext c
-    16 -> llvmI16TypeInContext c
-    32 -> llvmI32TypeInContext c
-    64 -> llvmI64TypeInContext c
-    _ -> llvmIntTypeInContext c (CUInt bits)
-
-mkPointerType :: Ptr Type -> IO (Ptr Type)
-mkPointerType pointeeTy =
-  llvmPointerTypeInContext pointeeTy 0
-
-mkStructType :: ForeignPtr Context -> [Ptr Type] -> Bool -> IO (Ptr Type)
-mkStructType ctx tys packed =
-  withForeignPtr ctx $ \c ->
-    withArray tys $ \tyArray -> do
-      let count = CUInt $ fromIntegral $ length tys
-          packed' = CBool (if packed then 1 else 0)
-      llvmStructTypeInContext c tyArray count packed'
-
-mkFunctionType :: Ptr Type -> [Ptr Type] -> IO (Ptr Type)
-mkFunctionType retTy argTys =
-  withArray argTys $ \argTyArray -> do
-    let argCount = CUInt $ fromIntegral $ length argTys
-        isVarArg = CBool 0
-    llvmFunctionTypeInContext retTy argTyArray argCount isVarArg
-
-mkArrayType :: Ptr Type -> Word32 -> IO (Ptr Type)
-mkArrayType elemTy count =
-  llvmArrayTypeInContext elemTy (CUInt count)
-
-mkContext :: IO (ForeignPtr Context)
-mkContext = mask_ $ do
-  ctx <- llvmContextCreate
-  newForeignPtr llvmContextDispose ctx
-
-mkModule :: ForeignPtr Context -> Text -> IO (ForeignPtr Module)
-mkModule ctx name =
-  withCString (T.unpack name) $ \name' -> do
-    withForeignPtr ctx $ \c -> mask_ $ do
-      llvmModule <- llvmCreateModuleWithName name' c
-      newForeignPtr llvmDisposeModule llvmModule
-
-getTargetData :: ForeignPtr Module -> IO (Ptr TargetData)
-getTargetData llvmModule =
-  withForeignPtr llvmModule llvmGetTargetData
-
-sizeOfType :: Ptr TargetData -> Ptr Type -> IO Word64
-sizeOfType td ty = do
-  CSize byteSize <- llvmSizeOfType td ty
-  pure byteSize
