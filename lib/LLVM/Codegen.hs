3c251c25b3a8fb1386b24930aa2a03b0ab4812a4
Luc Tielen <luc.tielen@gmail.com>
2022-05-25 23:20:10 +0200

    Cleanup top level module

--- lib/LLVM/Codegen.hs
+++ lib/LLVM/Codegen.hs
@@ -1,39 +1,22 @@
-{-# LANGUAGE RecursiveDo #-}
-
 module LLVM.Codegen
   ( module LLVM.Codegen.IRBuilder
   , module LLVM.Codegen.ModuleBuilder
-  , exampleModule -- TODO remove
+  , module LLVM.Codegen.NameSupply
+  , module LLVM.Codegen.Type
+  , module LLVM.Codegen.Operand
+  , module LLVM.Codegen.IR
+  , ppllvm
   ) where
 
 import LLVM.Codegen.NameSupply
 import LLVM.Codegen.IRBuilder
 import LLVM.Codegen.ModuleBuilder
 import LLVM.Codegen.Type
-
-import Control.Monad.IO.Class
-import qualified LLVM.C.API as C
-
-
-exampleModule :: IO Module
-exampleModule = runModuleBuilderT $ do
-  liftIO $ do
-    ctx <- C.mkContext
-    llvmMod <- C.mkModule ctx "test"
-    td <- C.getTargetData llvmMod
-    tys <- traverse (C.mkIntType ctx) [32, 64]
-    structTy <- C.mkStructType ctx tys False
-    size <- C.sizeOfType td structTy
-    print (structTy, size)  -- Expect (ptr 0x..., 8)
+import LLVM.Codegen.Operand
+import LLVM.Codegen.IR
+import LLVM.Pretty
+import Data.Text
 
 
-  function (Name "do_add") [IntType 1, IntType 32] (IntType 8) $ \[x, y] -> mdo
-    z <- add x y
-    _ <- add x y
-
-    _ <- block
-    _ <- add y z
-    ret y
-
--- $> Data.Text.IO.putStrLn =<< renderDoc <$> exampleModule
-
+ppllvm :: Module -> Text
+ppllvm = renderDoc
