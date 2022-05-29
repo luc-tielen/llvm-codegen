500f4826c36c495e4fc2b53afea22e2bebdd8e9f
Providence Salumu <Providence.Salumu@smunix.com>
2022-05-29 07:23:43 -0400

    nix/ghc: support ghc 902 + 922

--- flake.nix
+++ flake.nix
@@ -1,59 +1,99 @@
 {
-  description =
-    "llvm-codegen: LLVM code generation using Haskell";
-  inputs = {
-    np.url = "github:nixos/nixpkgs?ref=haskell-updates";
-    fu.url = "github:numtide/flake-utils?ref=master";
-    ds.url = "github:numtide/devshell?ref=master";
-  };
-  outputs = { self, np, fu, ds }:
-    with np.lib;
-    with fu.lib;
+  description = "llvm-codegen: LLVM code generation using Haskell";
+
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nix-filter.url = "github:numtide/nix-filter";
+
+  outputs = { self, flake-utils, nix-filter, devshell, nixpkgs }:
+    with nixpkgs.lib;
+    with flake-utils.lib;
     eachSystem [ "x86_64-linux" ] (system:
       let
-        ghcVersion = "902";
-        version = "${ghcVersion}.${substring 0 8 self.lastModifiedDate}.${
-            self.shortRev or "dirty"
-          }";
+        rmDot = replaceStrings [ "." ] [ "" ];
+        supportedGHCs = [ "default" "902" "922" ];
         config = { };
-        overlay = final: _:
+        overlays.devshell = devshell.overlay;
+        overlays.default = f: p:
           let
-            haskellPackages =
-              final.haskell.packages."ghc${ghcVersion}".override {
-                overrides = hf: hp: {
-                  llvm-codegen =
-                    (hf.callCabal2nix "llvm-codegen" ./. {
-                      llvm-config = final.llvmPackages_13.llvm;
-                    });
-                };
-              };
-          in { inherit haskellPackages; };
+            ghcVersion = "ghc${rmDot p.haskellPackages.ghc.version}";
 
-        pkgs = import np {
+            mkHaskellPackages = hspkgs:
+              hspkgs.extend (hf: hp:
+                with f.haskell.lib;
+                composeExtensions (hf: hp: {
+                  llvm-codegen = disableLibraryProfiling
+                    ((hf.callCabal2nix "llvm-codegen" (with nix-filter.lib;
+                      filter {
+                        root = self;
+                        exclude = [ (matchExt "cabal") ];
+                      }) { }).overrideAttrs (old: {
+                        version = "${rmDot hp.ghc.version}-${old.version}-${
+                            substring 0 8 self.lastModifiedDate
+                          }.${self.shortRev or "dirty"}";
+                      }));
+                }) (hf: hp: { llvm-config = f.llvmPackages_13.llvm; }) hf hp);
+
+            # all haskellPackages
+            allHaskellPackages = let
+              cases = listToAttrs (map (n: {
+                name = "${n}";
+                value = mkHaskellPackages
+                  f.haskell.packages."${if n == "default" then
+                    "${ghcVersion}"
+                  else
+                    "ghc${n}"}";
+              }) supportedGHCs);
+            in cases;
+
+            # all packages
+            allPackages = listToAttrs (map (n: {
+              name = if n == "default" then n else "llvm-codegen-${n}";
+              value = allHaskellPackages."${n}".llvm-codegen;
+            }) supportedGHCs);
+
+            # make dev shell
+            mkDevShell = g:
+              p.devshell.mkShell {
+                name = "llvm-codegen-${
+                    if g == "default" then "${ghcVersion}" else g
+                  }-${substring 0 8 self.lastModifiedDate}.${
+                    self.shortRev or "dirty"
+                  }";
+                packages = with f;
+                  with f.allHaskellPackages."${g}"; [
+                    ghcid
+                    llvmPackages_13.llvm.dev
+                    (ghcWithPackages (hp:
+                      with hp; [
+                        llvm-codegen
+                        ghc
+                        cabal-install
+                        haskell-language-server
+                        hpack
+                        hsc2hs
+                      ]))
+                  ];
+              };
+
+            # all packages
+            allDevShells = listToAttrs (map (n: {
+              name = "${n}";
+              value = mkDevShell n;
+            }) supportedGHCs);
+          in {
+            haskellPackages = allHaskellPackages.default;
+            inherit allHaskellPackages allDevShells allPackages;
+          };
+
+        pkgs = import nixpkgs {
           inherit system config;
-          overlays = [ overlay ds.overlay ];
+          overlays = [ overlays.devshell overlays.default ];
         };
+
       in with pkgs.lib; rec {
-        inherit overlay;
-        packages = { inherit (pkgs.haskellPackages) llvm-codegen; };
-        defaultPackage = packages.llvm-codegen;
-        devShell = pkgs.devshell.mkShell {
-          name = "llvm-codegen";
-          imports = [];
-          packages = with pkgs;
-            with haskellPackages; [
-              pkgs.llvmPackages_13.llvm.dev
-              pkgs.ghcid
-              (ghcWithPackages (p:
-                with p; [
-                  hspec-discover
-                  ghc
-                  cabal-install
-                  hsc2hs
-                  hpack
-                  haskell-language-server
-                ]))
-            ];
-        };
+        inherit overlays;
+        packages = flattenTree (pkgs.recurseIntoAttrs pkgs.allPackages);
+        devShells = flattenTree (pkgs.recurseIntoAttrs pkgs.allDevShells);
       });
 }
